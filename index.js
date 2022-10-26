9e5741b13730af426ef3e04382ce6874c12a9d39
Mahfuj-HSTU <rmahfujur16@gmail.com>
2022-10-26 20:23:43 +0600

    last final, get updeted

--- index.js
+++ index.js
@@ -7,12 +7,19 @@ app.use( cors() );
 
 const courses = require( './data/courses.json' )
 
+
 app.get( '/', ( req, res ) => {
     res.send( 'Education API Running' )
 } );
 
 app.get( '/courses', ( req, res ) => {
     res.send( courses )
+} )
+
+app.get( '/courses/:id', ( req, res ) => {
+    const id = req.params.id;
+    const selectedCourse = courses.find( course => course.id === id );
+    res.send( selectedCourse );
 } )
 
 
